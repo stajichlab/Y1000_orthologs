c8bcc45ebe81080a7e2b51a2d11f025445adf399
Jason Stajich <jasonstajich.phd@gmail.com>
2024-08-03 22:43:06 -0700

    download and uncompress

--- pipeline/00_download.sh
+++ pipeline/00_download.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash -l
-
-
+#SBATCH --out download.log -c 4 -N 1 -n 1
+# download
 curl -L -o y1000p_pep_files.tar.gz https://plus.figshare.com/ndownloader/files/40534997
+# uncompress
+pigz -dc y1000p_pep_files.tar.gz | tar xf -
 
